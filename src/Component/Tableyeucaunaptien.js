4f51c958d2f9dc4e89a2910b7c90e00199470d7d
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-11-27 22:49:43 +0700

    a

--- src/Component/Tableyeucaunaptien.js
+++ src/Component/Tableyeucaunaptien.js
@@ -1,39 +1,66 @@
 import React, { useEffect, useState } from 'react'
-import { getDatabase, ref, onValue, child, update } from 'firebase/database';
+import { getDatabase, ref, onValue } from 'firebase/database';
 import app from '../FireBase/FireBase';
 import Itemnaptienchuaxyly from '../Items/Itemnaptienchuaxyly';
 import Itemhoadonnaptiendaxylt from '../Items/Itemhoadonnaptiendaxylt';
-
+import moment from 'moment';
 
 const Tableyeucaunaptien = (props) => {
     var classVisible = props.cl;
     const [chose, setchose] = useState(1);
     const [List, setList] = useState([]);
-    class Hoadon {
-        constructor(val, key) {
-            this.val = val;
-            this.key = key
-        }
-    }
-    var haveLoad = 0;
-    var db = getDatabase(app);
+    const [Listshow, setListshow] = useState([]);
+    const [soft, setsoft] = useState(1)
+    const [render, setrender] = useState(1);
+    var arrsoft = List;
+
 
     useEffect(() => {
 
+        if (soft > 1) {
+            if (soft == 2) {
+                arrsoft.sort((a, b) => {
+                    let datea = moment(a.val.date, "DD/MM/YYYY hh:mm:ss");
+                    let dateb = moment(b.val.date, "DD/MM/YYYY hh:mm:ss");
+
+                    return dateb.toDate() - datea.toDate()
+                })
+            }
+            else {
+                arrsoft.sort((a, b) => {
+                    return b.val.cost - a.val.cost
+                })
+
+            }
+            setListshow(arrsoft)
+        } else {
+            setListshow(List);
+        }
+    }, [soft, List])
+
+
+
+
+
+    useEffect(() => {
+        class Hoadon {
+            constructor(val, key) {
+                this.val = val;
+                this.key = key
+            }
+        }
+
+        var db = getDatabase(app)
         onValue(ref(db, "/HoaDonNapTien/"), (snapShot) => {
             var arr = [];
             snapShot.forEach((child) => {
-
                 arr.push(new Hoadon(child.val(), child.key))
-
             })
             setList((a) => { return arr });
 
         })
-
-
-
     }, [])
+    var haveLoad = 0;
 
 
 
@@ -43,42 +70,62 @@ const Tableyeucaunaptien = (props) => {
 
 
 
-        <div className={classVisible} id="containerUser" style={{ marginTop: 40 }}>
+        <div className={classVisible} id="containerUser">
 
             <div className='cardChose'>
-                <span className={chose == 1 ? classspan : ''} onClick={(e) => {
+                <span className={chose === 1 ? classspan : ''} onClick={(e) => {
                     setchose(1)
                 }} >Chưa xử lý</span>
-                <span className={chose == 2 ? classspan : ''} onClick={(e) => {
+                <span className={chose === 2 ? classspan : ''} onClick={(e) => {
                     setchose(2);
                 }} >Đã xử lý</span>
 
             </div>
+
+            <div className='naptien-option'>
+                <span >Bạn muốn sắp xếp theo ?</span>
+                <select onChange={(e) => {
+                    setsoft(Number(e.target.value));
+                }}>
+                    <option value={1}>none</option>
+                    <option value={2}>Sắp xếp theo thời gian</option>
+                    <option value={3}>Sắp xép theo số tiền</option>
+                </select>
+            </div>
+
+
+
+
             <table id="tableUser" className="content-table">
+
+
                 <thead>
                     <tr>
                         <td>Stt</td>
                         <th>Tên người dùng</th>
                         <th>Thời gian yêu cầu</th>
                         <th>Số tiền</th>
-                        {chose == 1 && <th> </th>}
+                        {chose === 1 && <th> </th>}
                     </tr>
                 </thead>
                 <tbody>
-                    {chose == 1 ?
-                        List.map((value, index) => {
+                    
+                    {chose === 1 ?
+                        Listshow.map((value, index) => {
                             !value.val.trangThai && haveLoad++;
                             return !value.val.trangThai && <Itemnaptienchuaxyly index={index} value={value} key={index} />
                         })
                         :
-                        List.map((value, index) => {
-                            !value.val.trangThai && haveLoad++;
+                        Listshow.map((value, index) => {
+                            value.val.trangThai && haveLoad++;
                             return value.val.trangThai && <Itemhoadonnaptiendaxylt index={index} value={value} key={index} />
                         })
+
                     }
+
                     {
-                        chose == 1 ? haveLoad==0 && <tr><td style={{ columnSpan: 4, color: 'red' }} >Không có kết quả nào</td></tr> :
-                            haveLoad==0 && <tr><td style={{ columnSpan: 4, color: 'red' }} >Không có kết quả nào</td></tr>
+                        chose === 1 ? haveLoad === 0 && <tr><td style={{ columnSpan: 5, color: 'red' }} >Không có kết quả nào</td></tr> :
+                            haveLoad === 0 && <tr><td style={{ columnSpan: 4, color: 'red' }} >Không có kết quả nào</td></tr>
                     }
                 </tbody>
 
