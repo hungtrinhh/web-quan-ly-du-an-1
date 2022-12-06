00cc8a7b6d229bf539e28e189bc4bb13dec9dfdf
Hung di code dao <101351150+hungtrinhh@users.noreply.github.com>
2022-12-06 14:30:17 +0700

    Update ItemHoadonhengio.js

--- src/Items/ItemHoadonhengio.js
+++ src/Items/ItemHoadonhengio.js
@@ -1,52 +1,56 @@
 import { get, getDatabase, push, ref, set, update } from 'firebase/database';
 import moment from 'moment';
-import React from 'react'
+import React, { useEffect } from 'react'
 import app from '../FireBase/FireBase';
 
 const ItemHoadonhengio = (props) => {
 
     const { index, val, cl } = props;
-    var db = getDatabase(app)
+
+    useEffect(() => {
+        var db = getDatabase(app)
+
+        if (!val.success) {
+
+            var db = getDatabase(app);
+            var dateObject = moment(val.timeStart + ':00', "DD/MM/YYYY hh:mm:ss");
+            dateObject = dateObject.toDate();
+            var date = new Date();
+            var secondsElapsed = dateObject - date;
+            var reftoday = `${date.getFullYear()}/${date.getMonth() + 1}`
+
+            console.log('Đã set giờ bật máy :' + secondsElapsed);
+
+
+            var timeout = setTimeout(() => {
+               
+                update(ref(db, `HoaDonHenGio/${val.id}`), {
+                    success: true
+
+                })
+                push(ref(db, 'Hoadonchoigame/' + reftoday), {
+                    cost: val.cost,
+                    dateEnd: val.timeEnd + ':00',
+                    dateStart: val.timeStart + ':00',
+                    gameid: val.gameid.id + "",
+                    success: false,
+                    userid: val.userId.id,
+
+                })
+
+                update(ref(db, `Game/${val.gameid.id + ""}`), {
+                    trangThai: 'Đang được chơi'
+
+
+                })
+            }, secondsElapsed);
+        }
+        return () => clearTimeout(timeout);
+    }, []);
 
 
 
-    if (!val.success) {
 
-        var db = getDatabase(app);
-        var dateObject = moment(val.timeStart + ':00', "DD/MM/YYYY hh:mm:ss");
-        dateObject = dateObject.toDate();
-
-
-        var date = new Date();
-        var secondsElapsed = dateObject - date;
-        var reftoday = `${date.getFullYear()}/${date.getMonth() + 1}`
-
-        console.log('Đã set giờ bật máy :'+secondsElapsed);
-        setTimeout(() => {
-
-            push(ref(db, 'Hoadonchoigame/' + reftoday), {
-                cost: val.cost,
-                dateEnd: val.timeEnd+':00',
-                dateStart: val.timeStart+':00',
-                gameid: val.gameid.id+"",
-                success:false,
-                userid:val.userId.id,
-
-            })
-            update(ref(db,`HoaDonHenGio/${val.id}`),{
-                success:true
-
-            })
-            update(ref(db,`Game/${val.gameid.id+""}`),{
-                trangThai:'Đang được chơi'
-
-
-            })
-        }, secondsElapsed);
-
-
-
-    }
 
     return (
         <tr id="itemus1" className={cl}>
